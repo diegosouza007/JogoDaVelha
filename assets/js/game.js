f147c9932e5a7ae15bd8208154da9fd280899234
diegosouza007 <dsouza@yahoo.com>
2021-10-07 23:27:06 -0300

    adding basics functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-// Model
+// Model - MVC pattern
+
+let board = ['', '', '', '', '', '', '', '', ''];
+let playerTurn = 0;
+let flags = ['x', 'o'];
+
+function handleMove(position) {
+
+    board[position] = flags[playerTurn];
+
+    playerTurn === 0 ? playerTurn = 1 : playerTurn = 0;
+
+}
