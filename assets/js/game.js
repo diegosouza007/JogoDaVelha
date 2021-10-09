7a9dbea93cbb99f946d618b28e081f1572fdf7fd
diegosouza007 <dsouza@yahoo.com>
2021-10-08 22:44:27 -0300

    adding more functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,33 @@
 let board = ['', '', '', '', '', '', '', '', ''];
 let playerTurn = 0;
 let flags = ['x', 'o'];
+const winnerSequences = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+// Add the flags in the board and change the game turn
 
 function handleMove(position) {
-
     board[position] = flags[playerTurn];
-
     playerTurn === 0 ? playerTurn = 1 : playerTurn = 0;
+}
+
+// Change the main variables to default state
+
+function clearVariables() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    playerTurn = 0;
+}
+
+// Check if there is a winner
+
+function isWinner() {
 
 }
--- assets/js/interface.js
+++ assets/js/interface.js
@@ -1,1 +1,79 @@
-// Controller
+// Controller - MVC pattern
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleClick);
+    })
+})
+
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', resetGame);
+
+function handleClick(event) {
+
+    let cell = event.target;
+    let position = cell.getAttribute('id');
+
+    handleMove(position);
+    insertSymbolOnBoard();
+    toggleBoardHoverSymbol();
+
+}
+
+// Function on being called, put X or O when clicking on a cell 
+
+function insertSymbolOnBoard() {
+
+    let cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => {
+
+        let position = cell.getAttribute('id');
+        let flag = board[position];
+
+        if (flag != '') {
+            cell.classList.add(flag);
+        }
+    })
+}
+
+// Switch between X or O the hover effect in the cells 
+
+function toggleBoardHoverSymbol() {
+
+    let board = document.querySelector('.board');
+
+    if (board.classList.contains('x')) {
+        board.classList.remove('x');
+        board.classList.add('o');
+    } else {
+        board.classList.remove('o');
+        board.classList.add('x');
+    }
+
+}
+
+// Clear all cells and start a new game
+
+function resetGame() {
+
+    let board = document.querySelector('.board');
+    let cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('o');
+    })
+
+    if (board.classList.contains('o')) {
+        board.classList.remove('o');
+        board.classList.add('x');
+    }
+
+    clearVariables();
+
+}
